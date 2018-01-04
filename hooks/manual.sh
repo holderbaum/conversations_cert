ee61914cfdba17fcd264508d47eea6f6376a56c2
Jakob Holderbaum <jakob@holderbaum.io>
2018-01-04 12:39:19 +0100

    Add hydrated hook for manual DNS entries

---
+++ hooks/manual.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -eu
+
+# based on https://github.com/lukas2511/dehydrated/wiki/example-dns-01-nsupdate-script
+
+function task_deploy_challenge {
+  echo ""
+  echo "Add the following to the zone definition of ${1}:"
+  echo "_acme-challenge.${1}. IN TXT \"${3}\""
+  echo ""
+  echo -n "Press enter to continue..."
+  read tmp
+  echo ""
+}
+
+function task_clean_challenge {
+  echo ""
+  echo "Now you can remove the following from the zone definition of ${1}:"
+  echo "_acme-challenge.${1}. IN TXT \"${3}\""
+  echo ""
+  echo -n "Press enter to continue..."
+  read tmp
+  echo ""
+}
+
+function task_usage {
+  echo "usage: $0 .."
+  exit 1
+}
+
+cmd="${1:-}"
+shift || true
+case "$cmd" in
+  deploy_challenge) task_deploy_challenge "$@" ;;
+  clean_challenge) task_clean_challenge "$@" ;;
+  deploy_cert) ;;
+  unchanged_cert) ;;
+  *) task_usage ;;
+esac
